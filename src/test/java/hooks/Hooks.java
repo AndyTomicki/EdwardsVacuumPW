53843ba9a75c905bae2f142cbbf029fe069fe8c3
AndyT <andrzej.tomicki@gmail.com>
2024-05-07 14:20:29 +0200

    Refactor of tags use.
    Examples:
    Device:
    @Device:Windows
    @Device:Pixel5
    
    Browser:
    @Browser:WebKit
    @Browser:Chrome
    @Browser:Firefox

--- src/test/java/hooks/Hooks.java
+++ src/test/java/hooks/Hooks.java
@@ -6,9 +6,9 @@ import factory.DriverFactory;
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
 import io.cucumber.java.Scenario;
-import utils.WebActions;
 
 import java.nio.file.Paths;
+import java.util.Arrays;
 
 public class Hooks {
     public DriverFactory driverFactory;
@@ -16,15 +16,23 @@ public class Hooks {
 
     @Before
     public void launchBrowser(Scenario scenario) {
-        String browserName = WebActions.getProperty("browser");  //Fetching browser value from config file
-        String device = "Windows"; // default device
-        String tags = scenario.getSourceTagNames().toString();
-        if (tags.contains("@Device=")) {
-            device = tags.substring(tags.indexOf("@Device=")+8, tags.indexOf("]"));
-            System.setProperty("device", device);
+        String browser = "Chrome"; // default values
+        String device = "Windows";
+
+        for (Object o : Arrays.stream(scenario.getSourceTagNames().toArray()).toArray()) {
+            String tag = o.toString();
+            if (tag.contains("@Device:")) {
+                device = tag.substring(8);
+                System.setProperty("device", device);
+            }
+            if (tag.contains("@Browser:")) {
+                browser = tag.substring(9);
+
+            }
         }
+        System.out.println("Running on browser: " + browser + " under device: " + device);
         driverFactory = new DriverFactory();
-        page = driverFactory.initDriver(browserName); // Passing browser name to launch the browser
+        page = driverFactory.initDriver(browser.toLowerCase()); // Passing browser name to launch the browser
     }
 
     //After runs in reverse order so order=1 will run first
--- src/test/resources/features/VideoPlaylist.feature
+++ src/test/resources/features/VideoPlaylist.feature
@@ -1,13 +1,13 @@
 @Current
 Feature: Verify the functionalities of Video Playlist page
 
-  @Device=Windows
+  @Device:Windows
   Scenario: Verify User is able to perform actions on Web Elements in Video Playlist page
     When user goes to "https://www-uat.edwardsvacuum.com/en-uk/testing-video-playlist"
     Then user accepts cookies
     Then user clicks on element with text "MENU"
 
-  @Device=Pixel5
+  @Device:Pixel5
   Scenario: Verify User is able to perform actions on Web Elements in Video Playlist page on Pixel5
     When user goes to "https://www-uat.edwardsvacuum.com/en-uk/testing-video-playlist"
     Then user accepts cookies
